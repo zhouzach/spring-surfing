e8bfb40914e1384adc695416f8d84388a70876c9
zhouzach <769087026@qq.com>
2018-05-09 14:18:23 +0800

    REST API

--- src/main/java/org/rabbit/mvc/config/WebConfig.java
+++ src/main/java/org/rabbit/mvc/config/WebConfig.java
@@ -3,11 +3,18 @@ package org.rabbit.mvc.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.accept.ContentNegotiationManager;
+import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.BeanNameViewResolver;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 @Configuration
 @EnableWebMvc
@@ -26,5 +33,27 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
+    }
+
+    @Bean
+    public ViewResolver cnViewResolver(ContentNegotiationManager cnm) {
+        ContentNegotiatingViewResolver cnvr = new ContentNegotiatingViewResolver();
+        cnvr.setContentNegotiationManager(cnm);
+        return cnvr;
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaType.TEXT_HTML);
+    }
+
+    @Bean
+    public ViewResolver beanNameViewResolver() {
+        return new BeanNameViewResolver();
+    }
+
+    @Bean
+    public View spittles() { //define spittles as JSON View
+        return new MappingJackson2JsonView();
     }
 }
--- src/main/java/org/rabbit/mvc/web/SpittleController.java
+++ src/main/java/org/rabbit/mvc/web/SpittleController.java
@@ -3,15 +3,15 @@ package org.rabbit.mvc.web;
 import org.rabbit.mvc.Spittle;
 import org.rabbit.mvc.data.SpittleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-@Controller
+@RestController
 @RequestMapping("/spittles")
 public class SpittleController {
 
